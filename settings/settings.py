cdc5ba229a2f423d16488a8050ab8f8ea9b4a00f
Jay <jstary80@gmail.com>
2019-08-06 19:31:11 +0200

    fix settings.py

--- settings/settings.py
+++ settings/settings.py
@@ -7,8 +7,7 @@ DB_NAME = 'tacotron'
 DB_USER = 'postgres'
 DB_PASSWORD = 'password'
 
-ACCESS_KEY_ID = 'AKIAI4MZZHXKM6VHYHQA'
-ACCESS_KEY = '3I1ZxElc6PkqNZ2KpzgxOdQA7L5nIqXTgEojHj5G'
+
 
 '''
 Developer mode setting: True or False
